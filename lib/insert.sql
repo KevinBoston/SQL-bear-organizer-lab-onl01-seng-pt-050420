ce63f893a0801f376d39363e82f274196317f66f
Kevin Boston <kbos22@gmail.com>
2020-06-30 12:28:32 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (6, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (7, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (8, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
