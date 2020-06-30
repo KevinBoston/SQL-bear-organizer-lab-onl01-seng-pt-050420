5f328104929ecd97b6cd822b0ee38e6ba7b62d2c
Kevin Boston <kbos22@gmail.com>
2020-06-30 12:57:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears VALUES (1, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
-INSERT INTO bears VALUES (2, 'Rowdy', 3, 'Male', 'black', 'rowdy', false);
-INSERT INTO bears VALUES (3, 'Tabitha', 3, 'Female', 'brown', 'agreeable', true);
-INSERT INTO bears VALUES (4, 'Sergeant Brown', 3, 'Male', 'brown', 'tough', true);
-INSERT INTO bears VALUES (5, 'Melissa', 4, 'Female', 'brown', 'agreeable', false);
-INSERT INTO bears VALUES (6, 'Grinch', 3, 'Male', 'green', 'conniving', true);
-INSERT INTO bears VALUES (7, 'Wendy', 6, 'Female', 'grey', 'agreeable', true);
-INSERT INTO bears VALUES (8, NULL, 24, 'Female', 'brown', 'not agreeable', true);
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', 1);
+INSERT INTO bears VALUES (2, 'Rowdy', 3, 'Male', 'black', 'rowdy', 0);
+INSERT INTO bears VALUES (3, 'Tabitha', 3, 'Female', 'brown', 'agreeable', 1);
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 3, 'Male', 'brown', 'tough', 1);
+INSERT INTO bears VALUES (5, 'Melissa', 4, 'Female', 'brown', 'agreeable', 0);
+INSERT INTO bears VALUES (6, 'Grinch', 3, 'Male', 'green', 'conniving', 1);
+INSERT INTO bears VALUES (7, 'Wendy', 6, 'Female', 'grey', 'agreeable', 1);
+INSERT INTO bears VALUES (8, NULL, 24, 'Female', 'brown', 'not agreeable', 1);
