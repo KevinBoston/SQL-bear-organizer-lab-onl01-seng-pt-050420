c26d0e716e8bd125d53ca59a26cd753da14dc706
Kevin Boston <kbos22@gmail.com>
2020-06-30 12:46:03 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,5 @@
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (1, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
-INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (2, 'Rowdy', 3, 'Male', 'black', 'rowdy', false);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (3, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (4, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
 INSERT INTO bears (id, name, age, gender, color, temperament, alive) VALUES (5, 'Mr. Chocolate', 3, 'Female', 'brown', 'agreeable', true);
